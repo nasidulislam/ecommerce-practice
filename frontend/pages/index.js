9ba047cea02980654ecac655300447b4ec90d676
Nasidul Islam <nasidulislam@gmail.com>
2022-04-23 22:32:19 -0400

    create initial pages

---
+++ frontend/pages/index.js
@@ -0,0 +1,5 @@
+export default function IndexPage() {
+  return <div>
+    <p>Index page</p>
+  </div>
+}
---
+++ frontend/pages/sell.js
@@ -0,0 +1,5 @@
+export default function SellPage() {
+  return <div>
+    <p>Sell page</p>
+  </div>
+}
